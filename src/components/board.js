829bb3f8fdfbe133b36c93d9f386c23763b21814
kumarichhaya <chhaya18180@gmail.com>
2025-12-26 23:40:01 +0530

    first commit

---
+++ src/components/board.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function board() {
+  return (
+    <main id="site-main">
+      <h1 className="text-dark title">Birthday Remainder</h1>
+    </main>
+  );
+}
