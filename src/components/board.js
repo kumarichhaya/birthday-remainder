0e8a650a2fdaf08ed07095a23b6147bf3ed5614c
kumarichhaya <chhaya18180@gmail.com>
2025-12-28 09:43:12 +0530

    app done

--- src/components/board.js
+++ src/components/board.js
@@ -1,46 +1,62 @@
-import React from "react";
 import List from "./list";
 import "./board.css";
 
-// const Info = [
-//   {
-//     img: "",
-//     name: "Prerna",
-//     Birthday: "",
-//   },
-//   {
-//     img: "",
-//     name: "Harsimran",
-//     Birthday: "",
-//   },
-//   {
-//     img: "",
-//     name: "",
-//     Birthday: "",
-//   },
-//   {
-//     img: "",
-//     name: "",
-//     Birthday: "",
-//   },
-//   {
-//     img: "",
-//     name: "",
-//     Birthday: "",
-//   },
-//   {
-//     img: "",
-//     name: "",
-//     Birthday: "",
-//   },
-// ];
+const Info = [
+  {
+    img: "https://i.pinimg.com/736x/f4/70/86/f47086d17c2fcaeb24920e92db136ceb.jpg",
+    name: "Shashwat",
+    Birthday: "2000-08-16",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7JnxPtIQ0NnOlYmhJc5Q61rmpHlESBqx96w&s",
+    name: "Chhaya",
+    Birthday: "2002-08-18",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7JnxPtIQ0NnOlYmhJc5Q61rmpHlESBqx96w&s",
+    name: "snaya",
+    Birthday: "2002-12-31",
+  },
+];
 
-export default function board() {
+export default function Board() {
   return (
     <main id="site-main">
-      <h1 className="text-dark title">Birthday Remainder</h1>
-      <List></List>
-      <div className="board"></div>
+      <h1 className="text-dark title">Birthday Reminder</h1>
+      <div className="board">
+        <List info={Today(Info)} />
+        <h2 className="upcoming text-dark">Upcoming</h2>
+        <List info={Upcoming(Info)} />
+      </div>
     </main>
   );
+}
+
+function Today(person) {
+  const today = new Date();
+  const currentDay = today.getDate();
+  const currentMonth = today.getMonth();
+
+  return person.filter((data) => {
+    const birthday = new Date(data.Birthday);
+    return (
+      birthday.getDate() === currentDay && birthday.getMonth() === currentMonth
+    );
+  });
+}
+
+function Upcoming(person) {
+  const today = new Date();
+  const currentMonth = today.getMonth();
+  const currentDay = today.getDate();
+
+  return person.filter((data) => {
+    const birthday = new Date(data.Birthday);
+    const month = birthday.getMonth();
+    const day = birthday.getDate();
+
+    const diff = (month - currentMonth + 12) % 12;
+
+    return (diff > 0 && diff <= 2) || (diff === 0 && day > currentDay);
+  });
 }
--- src/components/list.js
+++ src/components/list.js
@@ -1,17 +1,36 @@
 import React from "react";
 
-export default function list() {
+export default function list({ info }) {
+  console.log(info);
+  return <ul>{iterate(info)}</ul>;
+}
+
+function iterate(data) {
+  if (!data) return;
   return (
-    <ul>
-      <li>
-        <div className="flex">
-          <img src="" alt="img" width={20} height={20} />
-          <div className="title">
-            <h3 className="name">Persone name</h3>
-            <h5 className="age">23years</h5>
-          </div>
-        </div>
-      </li>
-    </ul>
+    <>
+      {data.map((person, index) => {
+        Old(person.Birthday);
+        return (
+          <li key={index}>
+            <div className="flex">
+              <img src={person.img} alt="img" width={90} height={90} />
+              <div className="title">
+                <h3 className="name">{person.name}</h3>
+                <h5 className="age">{Old(person.Birthday)}</h5>
+              </div>
+            </div>
+          </li>
+        );
+      })}
+    </>
   );
+}
+
+// how old the person is
+function Old(personAge) {
+  let year = new Date(personAge).getFullYear();
+  let CurrentYear = new Date().getFullYear();
+  let age = CurrentYear - year;
+  return age;
 }
